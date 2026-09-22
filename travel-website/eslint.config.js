54883f0d8fd4efcd155935bb1bd6b220dabdc13d
Prince-Steele <princesteele23@gmail.com>
2026-09-21 21:19:14 -0500

    Increase supporting text size across travel site

--- travel-website/eslint.config.js
+++ travel-website/eslint.config.js
@@ -3,7 +3,7 @@ import react from "eslint-plugin-react";
 import globals from "globals";
 
 export default [
-  { ignores: ["dist/**", "node_modules/**", "showcase/**", "test-results/**"] },
+  { ignores: [".pms-repo/**", "dist/**", "node_modules/**", "showcase/**", "test-results/**"] },
   js.configs.recommended,
   {
     files: ["**/*.{js,jsx,mjs}"],
